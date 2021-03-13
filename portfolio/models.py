cb39bfc5647422c1676d5d1db1eac10810d8c8f1
alxanderapollo <alxanderapollo@gmail.com>
2021-03-13 12:25:45 -0500

    first commit

---
+++ portfolio/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# represent the portfolio projects
+#adding this to our data base
+class Project(models.Model):
+    #max_length - the title of projects can never have a charater length more than a 100
+    title = models.CharField(max_length=100)
+    #description of each project has a max of 250 chars
+    description = models.CharField(max_length=250)
+    #allows us to upload images, to specified folder
+    #in this case it goes to portfolio/images/
+    image = models.ImageField(upload_to='portfolio/images/')
+    #clickable on the portfolio
+    url = models.URLField(blank=True) #blank in this case means its
+    #not necssary to add an image with characters
+
+    
