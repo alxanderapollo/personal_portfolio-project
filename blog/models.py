10cf888453f87d5c3443bd82c90b2775ba950a17
alxanderapollo <alxanderapollo@gmail.com>
2021-03-14 13:57:58 -0400

    started blog posting

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    #max_length - the title of projects can never have a charater length more than a 100
+    title = models.CharField(max_length=100)
+    #description of each project has a max of 250 chars
+    date = models.CharField(max_length=250)
+    
+    #clickable on the portfolio
+    url = models.URLField(blank=True) #blank in this case means its
+    #not necssary to add an image with characters
+
+    
