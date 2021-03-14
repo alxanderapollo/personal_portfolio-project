3b43fe2e3cc3a052d220daf5118b2cb7e18bfa06
alxanderapollo <alxanderapollo@gmail.com>
2021-03-14 13:38:59 -0400

    added blog path and accesability

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+#adding a path so that we can access blog files
+from django.urls import include
+
+#dont need views because we are in the same folder can use only just . and itll be fine
+from . import views
+
+
+urlpatterns = [ 
+    path('', views.all_blogs, name = 'all_blogs'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # # Create your views here.
-# def blog (request):
-#     return render(request, 'blog/myblog.html')
+def all_blogs (request):
+    return render(request, 'blog/all_blogs.html')
